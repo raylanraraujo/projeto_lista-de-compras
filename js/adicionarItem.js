0cc2e893540cbca8cf3b5962984462f5db3be1a3
Raylan Araujo <raylanra@gmail.com>
2024-11-22 20:05:24 -0300

    Inserir feedback visual para lista vazia e Verificação de lista vazia

--- js/adicionarItem.js
+++ js/adicionarItem.js
@@ -1,14 +1,14 @@
+import { verificarListaVazia } from "./verificarListaVazia.js";
 import { criarItemDaLista } from "./criarItemDaLista.js";
 
 const item = document.getElementById("input-item");
-
 const listaDeCompras = document.getElementById("lista-de-compras");
-
 
 export function adicionarItem(evento) {
     evento.preventDefault()
-   
+    
     const itemDaLista = criarItemDaLista(item.value);  
     listaDeCompras.appendChild(itemDaLista);
+    verificarListaVazia(listaDeCompras);
 }
 
